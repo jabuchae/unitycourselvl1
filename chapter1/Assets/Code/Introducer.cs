b83ef40b437b9c18a2c7ef9ee43362d9eca07b8b
Jose Abuchaem <jose@evolve.xyz>
2020-02-25 17:22:29 -0300

    Lesson 6

---
+++ chapter1/Assets/Code/Introducer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introducer : MonoBehaviour
+{
+    public float speed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hi, my name is Red Car");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Transform>().Translate(new Vector3(-speed, 0, 0));
+        print("I'm at:" + gameObject.GetComponent<Transform>().position);
+    }
+}
