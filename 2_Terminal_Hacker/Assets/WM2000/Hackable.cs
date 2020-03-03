74f3597da0df188c18e22050b230ec0e10d9b073
Jose Abuchaem <jose@evolve.xyz>
2020-03-03 08:48:39 -0300

    Creating a story

--- 2_Terminal_Hacker/Assets/WM2000/Hackable.cs
+++ 2_Terminal_Hacker/Assets/WM2000/Hackable.cs
@@ -7,12 +7,11 @@ interface Hackable
     string GetName();
     string GetPasword();
     string GetWinMessage();
-    bool isEnabled();
 }
 
 class SiblingJournal : Hackable
 {
-    private const string name = "your sibling's online journal";
+    private const string name = "a random entry on your sibling's online journal";
     private Dictionary<string, string> passwords = new Dictionary<string, string>();
     private string[] passwordKeys;
     private readonly string[] winMessages = new string[]
@@ -26,21 +25,21 @@ class SiblingJournal : Hackable
     I went over to our neighbours for Margareth's birthday today.
     Can't believe she's 12 already!
 
-    Jenny came too so I wouldn't go alone.
+    Jenny came along too.
     We had a lot of fun and ate a lot of cake."
     );
 
         passwords.Add("David",
             @"Jul 17th, 2005
-    I don't understand what's going on with David.
+    I don't understand David.
     He's been avoiding me since that stupid argument with the Jeffersons.
 
     I don't care what he thinks.
-    They are nice people and they love their daughter."
+    They are nice people and they love Margareth."
     );
         passwords.Add("Mandy",
             @"Feb 2nd, 2006
-    I took Mandy over to the Jeffersons for Maggie's bday.
+    I took Mandy over to the Jeffersons for Margareth's birthday.
 
     It was nice. I like being around Mandy.
     She makes me happy."
@@ -58,8 +57,7 @@ class SiblingJournal : Hackable
     I'm borded. Just waiting for Lenny to go to Maggie's birthday.
 
     I just hope she's ok, I heard some screaming coming from her house...
-    Screw Lenny, I'm going over now to check on Maggie!
-"
+    Screw Lenny, I'm going over now to check on Maggie!"
     );
 
 
@@ -68,19 +66,8 @@ class SiblingJournal : Hackable
 
     private string[] RandomizePasswords()
     {
-        string[] keys = new string[5];
+        string[] keys = new string[passwords.Keys.Count];
         passwords.Keys.CopyTo(keys, 0);
-
-        for (int i = 0; i < 10; i++)
-        {
-            var key1 = Random.Range(0, keys.Length);
-            var key2 = Random.Range(0, keys.Length);
-
-            var aux = keys[key1];
-            keys[key1] = keys[key2];
-            keys[key2] = aux;
-        }
-        
 
         return keys;
     }
@@ -101,11 +88,6 @@ class SiblingJournal : Hackable
         passwordKeys = passwordKeys.Skip(1).ToArray(); // Remove the current key
 
         return passwords[currentPassword];
-    }
-
-    public bool isEnabled()
-    {
-        return passwordKeys.Length != 0;
     }
 }
 
@@ -133,10 +115,5 @@ class NeighbourWifi : Hackable
     {
         alreadyWon = true;
         return "You've hacked into your neighbourh's WiFi and you are now in their local network.";
-    }
-
-    public bool isEnabled()
-    {
-        return !alreadyWon;
     }
 }
--- 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -8,12 +8,17 @@ public class Hacker : MonoBehaviour
     private Hackable[] hackables;
 
     // Different states of the game
-    enum State { menu, hacking, ending }
+    enum State { menu, hacking, hackResult, journal, ending }
 
     // Game State
-    private State currentState = State.menu;
+    private State currentState;
     private string password;
     private int level;
+
+    private List<string> journalEntries;
+
+    private int[] levelsSolved;
+    private readonly int[] maxPuzzles = { 5, 1, 4 };
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +27,11 @@ public class Hacker : MonoBehaviour
             new SiblingJournal(),
             new NeighbourWifi()
         };
+
+        journalEntries = new List<string>();
+        currentState = State.menu;
+        levelsSolved = new int[] {0,0,0};
+
         ShowMainMenu();
     }
 
@@ -37,7 +47,7 @@ public class Hacker : MonoBehaviour
         Terminal.ClearScreen();
         Terminal.WriteLine("Feb 2nd, 2007");
         Terminal.WriteLine("    Boring day isn't it?");
-
+        Terminal.WriteLine("");
         ShowOptions();
     }
 
@@ -45,13 +55,30 @@ public class Hacker : MonoBehaviour
     {
         for (int i = 0; i < hackables.Length; i++)
         {
-            Terminal.WriteLine("");
-            Terminal.WriteLine("Press " + (i+1) + " for " + hackables[i].GetName());
+            if (levelsSolved[i] < maxPuzzles[i])
+            {
+                Terminal.WriteLine("Press " + (i + 1) + " to hack into " + hackables[i].GetName());
+                Terminal.WriteLine("");
+            }
         }
+
+        ShowMenuHint();
+    }
+
+    void ShowMenuHint()
+    {
+        string journalAccess = levelsSolved[0] >= 1 ? ", journal" : "";
+        Terminal.WriteLine("You can always type menu" + journalAccess + " or quit.");
     }
 
     void OnUserInput(string input)
     {
+        if (currentState == State.ending)
+        {
+            QuitGame();
+            return;
+        }
+
         if (ProcessGeneralCommand(input)) {
             return;
         }
@@ -63,12 +90,60 @@ public class Hacker : MonoBehaviour
             case State.hacking:
                 ProcessPasswordAttempt(input);
                 break;
-            case State.ending:
+            case State.journal:
+                ProcessJournalSelection(input);
+                break;
+            case State.hackResult:
                 break;
             default:
                 break;
         }
         
+    }
+
+    void QuitGame()
+    {
+        // save any game data here
+        #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+         Application.Quit();
+        #endif
+    }
+
+    void ProcessJournalSelection(string input)
+    {
+        foreach (string entry in journalEntries)
+        {
+            if (ClearFormat(input) == ClearFormat(GetFirstLine(entry)))
+            {
+                ShowJournalEntry(entry);
+                break;
+            }
+        }
+    }
+
+    string ClearFormat(string text)
+    {
+        var clearText = text.ToLower();
+        clearText = clearText.Replace("\n", "");
+        clearText = clearText.Replace(" ", "");
+        clearText = clearText.Replace(",", "");
+        clearText = clearText.Replace("2nd", "2");
+        clearText = clearText.Replace("20th", "20");
+        clearText = clearText.Replace("17th", "17");
+        
+        return clearText;
+    }
+
+    void ShowJournalEntry(string entry)
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(entry);
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Type menu to continue or journal to see more entries.");
     }
 
     void ProcessMenuInput(string input)
@@ -101,7 +176,38 @@ public class Hacker : MonoBehaviour
             return true;
         }
 
+        if (input == "journal" && levelsSolved[0] >= 1)
+        {
+            ShowJournalSelection();
+            return true;
+        }
+
+        if (input == "exit" || input == "quit" || input == "leave")
+        {
+            ShowEnding();
+            return true;
+        }
+
         return false;
+    }
+
+    void ShowJournalSelection()
+    {
+        currentState = State.journal;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Select the entry you want to revisit:");
+        foreach (string entry in journalEntries)
+        {
+            string firstLine = GetFirstLine(entry);
+            Terminal.WriteLine("    " + firstLine);
+        }
+
+        Terminal.WriteLine("");
+    }
+
+    string GetFirstLine(string text)
+    {
+        return text.Split('\n')[0];
     }
 
     void ShowLevelSelectionError()
@@ -118,7 +224,7 @@ public class Hacker : MonoBehaviour
         currentState = State.hacking;
         Terminal.ClearScreen();
         Terminal.WriteLine("Trying to hack " + hackables[level].GetName());
-
+        Terminal.WriteLine("Error: password could not be fully hacked.");
         password = GetPassword();
 
         ShowPasswordHint(password);
@@ -126,10 +232,10 @@ public class Hacker : MonoBehaviour
 
     void ShowPasswordHint(string password)
     {
-        Terminal.WriteLine("Password letters are:");
+        Terminal.WriteLine("Password retrieved but the letters are scrambled:");
         Terminal.WriteLine(password.Anagram());
         Terminal.WriteLine("");
-        Terminal.WriteLine("Input password:");
+        Terminal.WriteLine("Please input the correct password:");
     }
 
     string GetPassword()
@@ -152,17 +258,51 @@ public class Hacker : MonoBehaviour
     {
         Terminal.ClearScreen();
         Terminal.WriteLine("Password is not correct");
+        Terminal.WriteLine("You can always type menu to go back");
         Terminal.WriteLine("");
         ShowPasswordHint(password);
+        
     }
 
     void ShowVictory()
     {
-        Terminal.ClearScreen();
-        Terminal.WriteLine(hackables[level].GetWinMessage());
+        string winMessage = hackables[level].GetWinMessage();
 
+        levelsSolved[level] += 1;
+        if( level == 0)
+        {
+            journalEntries.Add(winMessage);
+        }
+
+        Terminal.ClearScreen();
+        Terminal.WriteLine(winMessage);
         Terminal.WriteLine("");
+        if(level == 0)
+        {
+            Terminal.WriteLine("Type journal to access all entries.");
+        }
         Terminal.WriteLine("Type menu to continue.");
+        currentState = State.hackResult;
+    }
+
+    void ShowEnding()
+    {
         currentState = State.ending;
+
+        Terminal.ClearScreen();
+        ShowUnknownEnding();
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press enter to quit the game");
+    }
+
+    void ShowUnknownEnding()
+    {
+        Terminal.WriteLine(@"You got bored and went playing outside.
+
+Your sibling didn't come back home that night. Or any night afterwards.
+
+You never really knew what happened.
+Maybe if you had listened to her more while she was around you'd know...");
+
     }
 }
