ee93c8973899efceb3104485caac531eeb2d8c04
Jose Abuchaem <jose@evolve.xyz>
2020-04-20 20:22:13 -0300

    Play thruster sound and fuel animation

--- Crazy Rocket/Assets/Scripts/Rocket.cs
+++ Crazy Rocket/Assets/Scripts/Rocket.cs
@@ -8,11 +8,15 @@ public class Rocket : MonoBehaviour
     const float ROTATION_SPEED = 100.0f;
 
     private Rigidbody rigidbody;
+    private AudioSource audioSource;
+    private ParticleSystem fire;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        fire = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -26,20 +30,29 @@ public class Rocket : MonoBehaviour
         var rotation = Time.deltaTime * ROTATION_SPEED;
         var movement = SPEED;
 
-        rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
 
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up * movement);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+                fire.Play(true);
+            }
+        } else
+        {
+            audioSource.Stop();
+            fire.Stop(true);
         }
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             rigidbody.transform.Rotate(Vector3.forward * rotation);
-            rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
         } else if(Input.GetKey(KeyCode.RightArrow)) {
             rigidbody.transform.Rotate(-Vector3.forward * rotation);
-            rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
         }
 
     }
