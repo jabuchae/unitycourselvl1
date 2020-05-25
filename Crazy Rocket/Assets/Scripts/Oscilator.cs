f6638154f105478f336d8a45e5d921a861ab8205
Jose Abuchaem <jose@evolve.xyz>
2020-05-24 21:50:12 -0300

    Better levels and lighting

---
+++ Crazy Rocket/Assets/Scripts/Oscilator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscilator : MonoBehaviour
+{
+    [SerializeField] private Vector3 movementVector;
+    [SerializeField] private float period = 1;
+
+    private float movementFactor = 0;
+    private Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Protect against period = 0
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
+
+        float cycle = Time.time / period;
+        float sinWave = Mathf.Sin(cycle * Mathf.PI * 2);
+
+        transform.position = initialPosition + movementVector * sinWave;
+    }
+}
