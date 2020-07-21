a900363fa12768f0fb5691ba40ab4f4e8bc8ff0c
Jose Abuchaem <jose@evolve.xyz>
2020-07-21 17:46:31 -0300

    End level boss

---
+++ GroundFox/Assets/Scripts/EndLevelBoss.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelBoss : MonoBehaviour
+{
+    [SerializeField] private GameObject minePrefab;
+    [SerializeField] private float timeBetweenMines = 1.5f;
+    [SerializeField] private float difficultyRamp = 0.05f;
+
+    private float mineTime;
+
+    private void Start()
+    {
+        mineTime = timeBetweenMines;
+    }
+
+    void Update()
+    {
+        mineTime -= Time.deltaTime;
+
+        if (mineTime <= 0)
+        {
+            mineTime = Mathf.Max(timeBetweenMines, 0f);
+            timeBetweenMines -= difficultyRamp;
+            CreateMine();
+        }
+    }
+
+    private void CreateMine()
+    {
+        var mine = Instantiate<GameObject>(minePrefab, new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+        mine.AddComponent<SelfDestruct>();
+    }
+}
